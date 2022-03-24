7edf76735d5f917d0a7111564faca1983d9771c8
Norman Smart <normansmart14@gmail.com>
2022-03-24 18:19:35 -0400

    commit tree 94e708378b195e04991536770240608059365d0e

---
+++ index.js
@@ -0,0 +1,42 @@
+function shout(string) {
+
+    return string.toUpperCase();
+
+
+}
+
+function whisper(string) {
+
+    return string.toLowerCase();
+}
+
+
+function logShout(string) {
+
+
+console.log('HELLO');
+
+}
+
+
+function logWhisper(string) {
+
+
+    console.log('hello');
+    
+}
+
+
+function sayHiToHeadphonedRoommate(string) {
+
+const string = "Hello!";
+
+console.log(string);
+console.log(string.toUpperCase());
+
+console.log(string === string.toUpperCase());
+
+
+}
+
+    
